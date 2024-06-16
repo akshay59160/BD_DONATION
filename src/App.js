d94d3bb48e1744a5fe14a81e25fa67ece92578f9
Akshay Raj <akshay59160raj@gmail.com>
2024-06-16 22:58:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Header from "./component/Header/Header";
+import Blood from "./component/Blood/Blood";
+import Registration from "./component/Registration/Registration" ;
+import Home from "./component/Home/Home";
+import Footer from "./component/Footer/Footer";
+import BloodRegistration from "./component/BloodRegistration/BloodRegistration"
+import BloodResult from "./component/Bloodresult/BloodResult"
+import CampRegistrations from "./component/CampRegistration/CampRegistration"
+import CampResult from "./component/CampResult/CampResult"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <BrowserRouter>
+    <Header/>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/Blood" element={<Blood/>}/>
+    <Route path="/Registration" element={<Registration/>}/>
+    <Route path="/BloodRegistration" element={<BloodRegistration/>}/>
+    <Route path="/BloodResult" element={<BloodResult/>}/>
+    <Route path="/CampRegistrations" element={<CampRegistrations/>}/>
+    <Route path="/CampResult" element={<CampResult/>}/>
+
+    </Routes>
+    <Footer/>
+  </BrowserRouter>
 }
 
 export default App;
